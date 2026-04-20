d2e98265dbed5e25c520ec43f74d936865640116
Lemoel Mendes <225293226+lemoelmendes@users.noreply.github.com>
2026-04-20 11:08:59 -0300

    feat: configure EF Core DbContext and ScheduleItems DbSet

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVP_Agenda.Models;
+
+namespace MVP_Agenda.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<ScheduleItem> ScheduleItems { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using MVP_Agenda.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 
